26a000e651182af1477a2b489e7b291e38e155b6
Abhishek Manral <72154862+manral01@users.noreply.github.com>
2024-01-25 12:20:44 +0530

    Delete 04_comparison.js

--- 04_comparison.js
+++
@@ -1,7 +0,0 @@
-console.log("2">1); // this is not a good practice to do ,data types should be same while comparing
-console.log(null>0);
-console.log(null==0);
-console.log(null>=0); // here the result is not predectable 
-
-console.log(undefined==0); 
-console.log("2"===2); // this will not be true as === this will check the data type as well 
